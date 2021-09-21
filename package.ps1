ab120b5b4f010c17d190a83e5017abcfb5586e6d
banalna <judas.priest999@gmail.com>
2021-09-21 18:15:51 +0300

    fix run script

--- package.ps1
+++ package.ps1
@@ -12,24 +12,3 @@ docker build -f docker/Dockerfile -t $image -t $latestImage .
 
 # Set environment variables
 $env:IMAGE = $image
-
-try {
-    # Workaround to remove dangling images
-    docker-compose -f ./docker/docker-compose.yml down
-
-    docker-compose -f ./docker/docker-compose.yml up -d
-
-    Start-Sleep -Seconds 10
-    # Invoke-WebRequest -Uri http://localhost:8080/heartbeat
-    # Invoke-WebRequest -Uri http://localhost:8080/v1/beacons/get_beacons -Method Post
-
-    Write-Host "The container was successfully built."
-    
-    # Save the result to avoid overwriting it with the "down" command below
-    $exitCode = $LastExitCode 
-} finally {
-    docker-compose -f ./docker/docker-compose.yml down
-}
-
-# Return the exit code of the "docker-compose.yml up" command
-exit $exitCode 
